a20bd2f021b3e0fa35a91a30772ad75fc143bae7
Jordan Nkunga <jnkunga@hotmail.com>
2024-11-15 12:04:08 +0100

    deposit interface creation

---
+++ BankApp/src/BankManagementSystem/Deposit.java
@@ -0,0 +1,65 @@
+package BankManagementSystem;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+
+public class Deposit extends JFrame implements ActionListener {
+
+	JTextField amount;
+	JButton deposit, back;
+	String pinnumber;
+	Deposit(String pinnumber){
+		this.pinnumber = pinnumber;
+		setLayout(null);
+		
+		ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
+		Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
+		ImageIcon i3 = new ImageIcon(i2);
+		JLabel image = new JLabel(i3);
+		image.setBounds(0,0,900,900);
+		add(image);
+		
+		JLabel text = new JLabel("Enter the amout you want to deposit");
+		text.setForeground(Color.WHITE);
+		text.setFont(new Font("System", Font.BOLD,16));
+		text.setBounds(170,300,400,20);
+		image.add(text);
+		
+		JTextField amount = new JTextField();
+		amount.setFont(new Font("Raleway",Font.BOLD,22));
+		amount.setBounds(170,350,320,25);
+		image.add(amount);
+		
+		deposit = new JButton("Deposit");
+		deposit.setBounds(355,485,150,30);
+		deposit.addActionListener(this);
+		image.add(deposit);
+		
+		back = new JButton("Back");
+		back.setBounds(355,520,150,30);
+		back.addActionListener(this);
+		image.add(back);
+		
+		setSize(900,900);
+		setLocation(300,0);
+		setVisible(true);
+	}
+	public void actionPerformed(ActionEvent ae) {
+		if(ae.getSource()== deposit) {
+			
+		} else if (ae.getSource()== back){
+			setVisible(false);
+			new Transactions(pinnumber).setVisible(true);
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		new Deposit("");
+
+	}
+
+}
